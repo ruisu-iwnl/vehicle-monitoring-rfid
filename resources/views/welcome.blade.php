ef5c173e9a1f0515e95c34843193e3e3e6d125a4
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-07-24 15:37:01 +0800

    design added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
     <body class="bg-gray-100 h-screen flex items-center justify-center">
     <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <main class="py-4 text-center">
+    <main class="py-4 text-center">
 
     <style>
         .button {
@@ -44,7 +44,7 @@
 
 
             @yield('content')
-        </main>
+    </main>
     </div>
 
     @livewireScripts 
